d6d407f4d4ea9e4cc75d7783e5911ceeec5b5486
MalikUmais <umaisfaruq03@gmail.com>
2025-08-04 20:21:54 +0500

    comment system setup

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4806329a1fa520628382ffbe37a8342bb605f118")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c57c9b72a7f54f5b293ef0bb266bb099b33aa1ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
