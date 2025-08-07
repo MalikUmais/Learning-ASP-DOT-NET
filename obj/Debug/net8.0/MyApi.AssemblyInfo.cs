5f4f071cd9601cda8774da4034b1f4af6de5ba4b
MalikUmais <umaisfaruq03@gmail.com>
2025-08-07 11:54:43 +0500

    git igonre file

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2be8b432e79f47f140945753d3dba80ff5f41b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c529e99cf75584070b03fde347f70e9a55d20a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
