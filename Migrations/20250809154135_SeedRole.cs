87bc13b138781d908be5f40bd9ba29d2ec7a6644
MalikUmais <umaisfaruq03@gmail.com>
2025-08-09 20:47:32 +0500

    added rergister

---
+++ Migrations/20250809154135_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "6be5033a-cf69-4273-809b-81de1976f6ea", null, "Admin", "ADMIN" },
+                    { "de6ac447-f0ad-4c22-9c0e-5e2c35daa80c", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6be5033a-cf69-4273-809b-81de1976f6ea");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "de6ac447-f0ad-4c22-9c0e-5e2c35daa80c");
+        }
+    }
+}
