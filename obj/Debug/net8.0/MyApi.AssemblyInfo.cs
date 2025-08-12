f826264ce5b1c296c74562527eb9ad1442bd4f89
MalikUmais <umaisfaruq03@gmail.com>
2025-08-12 20:06:43 +0500

    portfolio get setup

--- obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a8da0aea0c194cf4ebc65f6aed26e0cf7e6951")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e04598e680a1bac84ff0dda75d50f32b470e516e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
